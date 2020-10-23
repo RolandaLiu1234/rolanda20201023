be26aad2566655a9682f8f8e0b12282153865170
RolandaLiu1234 <72953853+RolandaLiu1234@users.noreply.github.com>
2020-10-23 10:41:04 +0900

    Add files via upload

---
+++ 20201019-2.py
@@ -0,0 +1,27 @@
+class Student:
+    def __init__(self,name):
+        def calculate_avg(self,score):
+            sum=0
+
+            for num in score:
+                sum+=num
+
+avg=sum/len(score)
+return avg
+
+def judge(self,avg):
+
+if avg>=60:
+    result="passed"
+    else:
+        result="failed"
+        return result
+
+        a001=Student("Sato")
+        score=(70,60,88,90,40)
+
+        avg=a001.calcutate_avg(score)
+        result=a001.judge(avg)
+
+        print(avg)
+        print(a001.name+""+result)
---
+++ 20201020-1.py
@@ -0,0 +1,11 @@
+class Student:
+
+    def __init__ (self):
+        self.name = ""
+      
+def avg(self,kokubun,sannsu,eigo,rekishi,rikei):
+    print((kokubun+sannsu+eigo+rekishi+rikei)/5)
+
+a001=Student("sato")
+print(a001.name)
+
